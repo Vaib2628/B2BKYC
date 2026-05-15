d2eee9592163fe90ac1c47f6636b248b34e00707
vaibhav <vaibhavpatil2579@gmail.com>
2026-05-15 15:52:20 +0530

    fixed me api

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ var router = express.Router();
 
 router.use("/auth", require("./auth"));
 router.use("/businesses", require("./businesses"));
-// router.use("/kyc", require("./kyc"))
+router.use("/kyc", require("./kyc"))
 
 module.exports = router;
