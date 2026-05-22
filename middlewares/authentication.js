f133589072d048f8aea8d9580c492630efe25ca2
vaibhav <vaibhavpatil2579@gmail.com>
2026-05-22 11:10:18 +0530

    fix(auth): Fixed the issue of businessId being underfined in the req.user

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -23,7 +23,7 @@ module.exports = async (req, res, next) => {
 
         req.user = {
             _id: decoded._id,
-            businessId: decoded.businessId,
+            businessId: membership.businessId,
             membershipId: membership._id,
             roleId: membership.roleId,
             scope: membership.scope
