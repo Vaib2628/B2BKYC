6fad192b5bb4bdd92269133706a5e4512b3ece5a
vaibhav <vaibhavpatil2579@gmail.com>
2026-06-01 08:58:33 +0530

    feat(trustscore): Implement trust score retrieval endpoint with authentication

---
+++ controllers/trustscore/getTrustScore.js
@@ -0,0 +1,15 @@
+const createHttpError = require("http-errors");
+const Business = require("../../models/Business");
+const TrustScoreHistory = require("../../models/TrustScoreHistory")
+const { STATUS_CODES, ERROR_MESSAGES } = require("../../constants/errorConstants");
+
+module.exports = async ({ businessId }) => {
+    const business = await Business.findById(businessId);
+    if (!business) throw new createHttpError(STATUS_CODES.NOT_FOUND, ERROR_MESSAGES.BUSINESS_NOT_FOUND)
+
+    const trustScoreHistory = await TrustScoreHistory.find({businessId}).select({businessId: 0, _id: 0});
+    return {
+        trustScore: business.trustScore,
+        history: trustScoreHistory
+    }
+};
--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,6 @@ router.use("/kyc", require("./kyc"))
 router.use("/roles", require("./roles"))
 router.use("/permissions", require("./permissions"))
 router.use("/deals",require("./deals"))
+router.use("/trustscore", require("./trustscore"))
 
 module.exports = router;
