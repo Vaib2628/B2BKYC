95d8930a6d4f91c6f87ff3d89380d5d383330488
vaibhav <vaibhavpatil2579@gmail.com>
2026-06-18 15:05:20 +0530

    feat(redis): add Redis configuration and dependency

---
+++ config/redis.js
@@ -0,0 +1,7 @@
+const { Redis } = require("ioredis");
+
+module.exports = new Redis({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD
+});
